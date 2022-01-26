c8412dc92eb0d8c06c526b8f2ed5d974eecd9d37
William Stull II <williamstull@gmail.com>
2022-01-25 18:27:59 -0800

    Updates

---
+++ precache-manifest.896a8d62efe35b4e8a6b313985dce6b0.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52b51657e6bbb5581ffd1c48dd9e448f",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "d31324cdbe8fd3d42d11",
+    "url": "/portfolio/static/css/2.309bc4dc.chunk.css"
+  },
+  {
+    "revision": "653fb53ce06b854c8b61",
+    "url": "/portfolio/static/css/3.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "f9fa6777554e21481357",
+    "url": "/portfolio/static/css/4.22ca87da.chunk.css"
+  },
+  {
+    "revision": "2705e2393b9ce1840fe4",
+    "url": "/portfolio/static/css/6.d95300ec.chunk.css"
+  },
+  {
+    "revision": "6d09c30321949fbd9417",
+    "url": "/portfolio/static/css/main.458959cb.chunk.css"
+  },
+  {
+    "revision": "d31324cdbe8fd3d42d11",
+    "url": "/portfolio/static/js/2.5be81fab.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/portfolio/static/js/2.5be81fab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "653fb53ce06b854c8b61",
+    "url": "/portfolio/static/js/3.04cbee79.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/portfolio/static/js/3.04cbee79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9fa6777554e21481357",
+    "url": "/portfolio/static/js/4.92925ab7.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/portfolio/static/js/4.92925ab7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3930ad6ebbffef0b24d",
+    "url": "/portfolio/static/js/5.695bef34.chunk.js"
+  },
+  {
+    "revision": "2705e2393b9ce1840fe4",
+    "url": "/portfolio/static/js/6.2b4c4e9e.chunk.js"
+  },
+  {
+    "revision": "205e9432990906e67b93",
+    "url": "/portfolio/static/js/7.7c409084.chunk.js"
+  },
+  {
+    "revision": "56b8ff1bec8a53eeb210",
+    "url": "/portfolio/static/js/8.04478947.chunk.js"
+  },
+  {
+    "revision": "eead6024ad55b2d00e4d",
+    "url": "/portfolio/static/js/9.fc7f773f.chunk.js"
+  },
+  {
+    "revision": "6d09c30321949fbd9417",
+    "url": "/portfolio/static/js/main.12d74a55.chunk.js"
+  },
+  {
+    "revision": "95d62eb2373fb23811c4",
+    "url": "/portfolio/static/js/runtime-main.b6c00f5b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b1ff1dd0dd8b317f6d0dd9b0ee20c345.js"
+  "/portfolio/precache-manifest.896a8d62efe35b4e8a6b313985dce6b0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
